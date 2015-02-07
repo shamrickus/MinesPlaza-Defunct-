40338b28e00f7fd8bd929c2865b19586e4e936ce
Colton Thomas <colthoma@mines.edu>
2015-02-07 12:14:14 -0700

    headers on board

--- files/accounts.php
+++ files/accounts.php
@@ -3,6 +3,9 @@
 		<title>Mines Plaza: Accounts</title>
 	</head>
 	<body>
+		<?php
+  			include 'header.php'
+  		?>
 		<h1:align="center">Accounts</h1>
 	</body>
 
--- files/sell.php
+++ files/sell.php
@@ -3,6 +3,9 @@
 		<title>Mines Plaza: Sell</title>
 	</head>
 	<body>
+		<?php
+  			include 'header.php'
+  		?>
 		<h1:align="center">Sell</h1>
 	</body>
 
