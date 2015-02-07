f83e8798be8b8cb68ebe885df95a580235b0f81d
Steve Hamrick <shamrick@mymail.mines.edu>
2015-02-07 12:03:49 -0700

    Logging in working

--- files/login_processing.php
+++ files/login_processing.php
@@ -1,25 +1,21 @@
 <?php
-require('../includes/variables.php');
+require_once('../includes/variables.php');
 require('../includes/login_func.php');
 
-$username = $_REQUEST['login_username'];
-$password = $_REQUEST['login_password'];
-
-$mysqli = CreateDBObject();
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
 
 //NEED TO DO SANITIZATION BEFORE DOING DB STUFF
-
-$result = $mysqli->query('SELECT password, id FROM users WHERE username = "'.$username.'"');
-if ($row = $result->fetch_assoc())
-{
-	if(password_verify($password, $row['password'])){
-    	header('Location: login.php?msg=Invalid Username/Password');
-	}
-	else{
-		createSession($row['id']);
-		header('Location: test.php?msg=Successfully logged in.');
-	}
-}else{
-	header('Location: login.php?msg=Invalid Username/Password');
+$query = 'SELECT * FROM users WHERE username = "'.$username.'"';
+if($stmt = $mysqli->prepare($query)){
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    if(!password_verify($password, $row['password'])) header('Location: login.php?msg=Invalid Username/Password');
+    else{
+        createSession($row['id']);
+        header('Location: home.php?msg=Successfully logged in.');
+    }
 }
+else header('Location: login.php?msg=Invalid Username/Password');
 ?>
--- includes/common_func.php
+++ includes/common_func.php
@@ -1,7 +1,13 @@
 <?php
 
 function random($length){
-    return $length;
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }
 
 ?>
