310b1ec4acce02c00f1d708703c6c73db3083350
ocket8888 <ocket8888@gmail.com>
2015-02-07 12:44:15 -0700

    Finished preliminary header (for real this time)

--- files/header.php
+++ files/header.php
@@ -19,7 +19,7 @@
       <a class="navbar-brand" href="#">Logo</a>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
+    <!-- Colleaeonstiasuetnct the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="home.php">Home</a></li>
