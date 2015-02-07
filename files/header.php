b3fcf07fdbf5499940c493503e10185c68c6f190
ocket8888 <ocket8888@gmail.com>
2015-02-07 14:26:36 -0700

    Put dildos on the buyers page

--- files/header.php
+++ files/header.php
@@ -26,7 +26,7 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Browse <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a style="text-align:center;" href="#">Buyers</a></li>
+            <li><a style="text-align:center;" href="buy-browse.php">Buyers</a></li>
             <li class="divider"></li>
             <li><a style="text-align:center;" href="#">Sellers</a></li>
           </ul>
