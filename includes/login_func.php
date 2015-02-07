36970b3825b83395658612d4807c30992e71f518
Steve Hamrick <shamrick@mymail.mines.edu>
2015-02-07 12:39:33 -0700

    Log in check working

--- includes/login_func.php
+++ includes/login_func.php
@@ -12,29 +12,29 @@ function createSession($userid){
     $mysqli = createDBObject();
     
     $result = $mysqli->query('DELETE FROM user_session WHERE user_id='.$userid);
-    $query = "INSERT INTO user_session (user_id, session_id, expire_time)
-				VALUES ( '.$userid.', '.$sid.', '.$time.')";
+    $query = 'INSERT INTO user_session (user_id, session_id, expire_time)
+				VALUES ( '.$userid.', "'.$sid.'", '.$time.')';
 	if ($stmt = $mysqli->prepare($query)){
 		$stmt->execute();
     }
     else{
         logout($userid);
     }
-    
-    return true;
 }
 
 //Check if a user is logged in
- function loggedIn($userid = ''){
-    if(strcmp($userid, '')) $userid = $_COOKIE['SessionUser'];
-    $query = "SELECT FROM user_session WHERE user_id='.$userid";
-     if($stmt = $mysqli->prepare($query)){
+ function loggedIn($userid = 0){
+     $mysqli = createDBObject();
+    if($userid == 0) $userid = $_COOKIE['SessionUser'];
+    $query = "SELECT * FROM user_session WHERE user_id=".$userid;
+    if($stmt = $mysqli->prepare($query)){
         $stmt->execute();
-        $row = $stmt->fetch_assoc();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
         if(strcmp($_SESSION['SessionID'], $row['session_id'])){
             if(time() < $row['expire_time']){
                 $time = time() + 3600;
-                $query = "UPDATE user_session SET exipre_time = ".$time." WHERE user_id = ".$userid;
+                $query = "UPDATE user_session SET expire_time = ".$time." WHERE user_id = ".$userid;
                 if($stmt = $mysqli->prepare($query)){
                     $stmt->execute();
                 }
@@ -48,9 +48,9 @@ function createSession($userid){
 }
 
 //Logout given user or logout user based on cookies
-function logout($userid = ''){
+function logout($userid = 0){
     $mysqli = createDBObject();
-    if(strcmp($userid, '')) $userid = $_COOKIE['SessionUser'];
+    if($userid == 0) $userid = $_COOKIE['SessionUser'];
     $result = $mysqli->query('DELETE FROM user_session WHERE user_id='.$userid);
     setcookie('SessionUser', 0, 1);
     setcookie('SessionId', 0, 1);
