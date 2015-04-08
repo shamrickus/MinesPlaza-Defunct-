469b8eae17095ff11ccde349937d11eec0670567
root <ocket8888@gmail.com>
2015-04-07 18:12:43 -0600

    idk

---
+++ files/modpage.php
@@ -0,0 +1,16 @@
+<!--DOCTYPE HTML-->
+<html>
+<head>
+
+</head>
+<body>
+<?php
+	include 'header.php'
+?>
+<!-- if this joker's not an admin (don't have a database, so I don't know how to do that)
+
+
+-->
+
+</body>
+</html>
